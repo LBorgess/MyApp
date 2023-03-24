ca5cbec5150c8020839cf660fdf8859fb41aba06
LBorgess <lucas_rancer@hotmail.com>
2023-03-23 22:04:21 -0300

    Ações

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -2,13 +2,27 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btnAbrirSlpash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        btnAbrirSlpash = findViewById(R.id.btnAbrirSplash);
+
+        btnAbrirSlpash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), SplashActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/br/com/etecia/myapp/SplashActivity.java
+++ app/src/main/java/br/com/etecia/myapp/SplashActivity.java
@@ -2,9 +2,11 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class SplashActivity extends AppCompatActivity {
     // Declarando o Objeto botão
@@ -21,7 +23,7 @@ public class SplashActivity extends AppCompatActivity {
         btnAbrirLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
             }
         });
 
