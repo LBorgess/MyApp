1634bf2433303481a690093293cec015145a8e42
LBorgess <lucas_rancer@hotmail.com>
2023-03-23 22:07:51 -0300

    Torrada

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -22,6 +23,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), SplashActivity.class));
+                Toast.makeText(getApplicationContext(), "Clicado", Toast.LENGTH_SHORT).show();
             }
         });
     }
