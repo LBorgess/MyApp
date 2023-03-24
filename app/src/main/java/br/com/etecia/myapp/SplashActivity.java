0f4d49e3bfa90fc531113bc3a4a4790412b4365a
LBorgess <lucas_rancer@hotmail.com>
2023-03-23 21:46:18 -0300

    Button

--- app/src/main/java/br/com/etecia/myapp/SplashActivity.java
+++ app/src/main/java/br/com/etecia/myapp/SplashActivity.java
@@ -3,12 +3,20 @@ package br.com.etecia.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SplashActivity extends AppCompatActivity {
+    // Declarando o Objeto botão
+    Button btnAbrirLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
+        // Apresentando o XML para o Java
+        btnAbrirLogin = findViewById(R.id.btnAbrirLogin);
+
+        // Criar a ação de clique do botão
+
     }
 }
